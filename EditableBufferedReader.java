38cc8e8f108919f79bf76d2d0624de6070d77cea
Genis <145459716+gemigu@users.noreply.github.com>
2023-10-17 15:53:45 +0200

    Update EditableBufferedReader.java

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -3,11 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package EditableBufferedReader;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+
 
 /**
  *
@@ -17,14 +16,21 @@ public class EditableBufferedReader extends BufferedReader {
     
     private static final int ESCAPE_KEY=27;
     private static final int BRACKET_KEY=91;
-    
+    private static final int LEFT_KEY=68;
+    private static final int RIGHT_KEY=67;
+    private static final int HOME_KEY=72;
+    private static final int END_KEY=70;
+    private static final int INS_KEY=50;
+    private static final int DEL_KEY=51;
+    private static final int BACKSPACE_KEY=127;
+
+
     private boolean isRawmode=false;
-    private boolean overwriteMode=false;
-    private StringBuilder line;
-    private int cursorPosition;
+    private Line line;
     
     public EditableBufferedReader(Reader in) {
         super(in);
+        line= new Line();
     }
     
     public void setRaw() throws IOException{
@@ -60,7 +66,24 @@ public class EditableBufferedReader extends BufferedReader {
     
     private void realitzarSequenciaEscape(int action){
         switch (action){
-            case 'C': //flecha dreta
+            case LEFT_KEY: //flecha dreta
+            line.moveLeft();
+            break;
+            case RIGHT_KEY:
+            line.moveRight(); 
+            break;   
+            case HOME_KEY:
+            line.moveToBeginning();
+            break;   
+            case END_KEY:
+            line.moveToEnd(); 
+            break;   
+            case INS_KEY:
+            line.toggleInsertMode();
+            break;
+            case DEL_KEY:
+            line.backspace();
+            break;
                 
                 
         }
@@ -68,24 +91,14 @@ public class EditableBufferedReader extends BufferedReader {
     }
     
     private void realitzarCaracterNormal(int c){
-        
-    }
-    
-    public String readLine() throws IOException{
-        StringBuilder line = new StringBuilder();
-        int c;
-        try{
-            while((c=read())!=-1){
-                if(c=='\r'){
-                    
-                }else if (c== )
-                
-            }
+        if(c==BACKSPACE_KEY){
+            line.backspace();
+        }else{
+        line.insert((char) c);
         }
     }
     
-
-    
-    
-    
+    public String readLine() throws IOException{
+        return line.getText();
+    }
 }
