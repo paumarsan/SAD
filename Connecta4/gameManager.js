092cd4314c9e86057a0682a6a9975a431598b75f
Genis <145459716+gemigu@users.noreply.github.com>
2023-12-10 16:19:48 +0100

    Update gameManager.js

--- Connecta4/gameManager.js
+++ Connecta4/gameManager.js
@@ -4,6 +4,7 @@ const joc = {
   jugadors: [],
   tauler: inicialitzarTauler(),
   tornJugador: 0,
+  finpartida: false,
 };
 
 
@@ -26,14 +27,16 @@ function handleConnection(socket, io) {
   });
 
   socket.on('ferMoviment', ({ columna }) => {
-    if (joc.jugadors.length === 2 && joc.tornJugador === joc.jugadors.findIndex(player => player.id === socket.id)) {
+    if (joc.finpartida === false && joc.jugadors.length === 2 && joc.tornJugador === joc.jugadors.findIndex(player => player.id === socket.id)) {
       if (!columnaPlena(joc.tauler, columna)) {
         tirarPeça(joc.tauler, columna, joc.tornJugador + 1);
         io.emit('actualitzarJoc', joc);
         const result = checkVictoria(joc.tauler, joc.tornJugador + 1);
         if (result === 'tie') {
+          joc.finpartida=true;
           io.emit('gameOver', { winner: 'empat' });
         } else if (result) {
+          joc.finpartida=true;
           const jugadorGuanyador = joc.jugadors.find(player => player.id === socket.id);
           io.emit('gameOver', { winner: jugadorGuanyador});
         } else {
@@ -48,17 +51,19 @@ function handleConnection(socket, io) {
 
   socket.on('reiniciarJoc', () => {
     // Reiniciar la partida
+      joc.finpartida=false;
       joc.tornJugador = 0;
       joc.tauler = inicialitzarTauler();
       io.emit('actualitzarJoc', joc);
       io.emit('amagarResultat');
+
   });
 
-  socket.on('disconnect', () => 
-    // Administrar la desconnexió d'un jugador
+  socket.on('disconnect', () => {
     console.log(`Client desconnectat: ${socket.id}`);
     joc.jugadors=joc.jugadors.filter((player) => player.id !== socket.id);
     if(joc.jugadors.length === 0){
+      joc.finpartida=false;
       joc.tauler=inicialitzarTauler();
       joc.tornJugador=0;
     }
