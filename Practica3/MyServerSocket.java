56b556fad3ed36525346b1cd01adb7c8e79c1ebb
Genis <145459716+gemigu@users.noreply.github.com>
2023-12-27 20:19:43 +0100

    Update MyServerSocket.java

--- Practica3/MyServerSocket.java
+++ Practica3/MyServerSocket.java
@@ -1,1 +1,23 @@
+import java.net.*;
 
+public class MyServerSocket {
+    ServerSocket serversocket;
+
+    public MyServerSocket(){
+        try{
+            serversocket = new ServerSocket(8080);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public MySocket accept(){
+        try{
+            Socket sc = serversocket.accept();            
+            return new MySocket(sc);                        
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
