daaf602b9fba3eaadc270e9002981cdd4100b469
Genis <145459716+gemigu@users.noreply.github.com>
2023-12-27 20:59:31 +0100

    Update Client.java

--- Practica2/Client.java
+++ Practica2/Client.java
@@ -9,32 +9,24 @@ public class Client {
   public static void main(String[] args) throws IOException {
     MySocket mySocket = new MySocket("localhost", PORT);
 
-    new Thread(() -> { //Thread para leer de teclado y enviar hacia servidor
-     
+    new Thread(() -> {
         String input;
-        
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
           try {
             while ((input = in.readLine()) != null) {
               mySocket.printLine(input);
           }
-          
           mySocket.close(); 
         }catch(IOException e) {
               e.printStackTrace();
         }
-    
     }).start(); 
-  
-    new Thread(() -> {  //Thread para leer del servidor y printar por pantalla
-
+    
+    new Thread(() -> { 
         String output;
-
         while ((output = mySocket.readLine()) != null) {  
           System.out.println(output);
       	}
-
     }).start();
-
   }
 }
