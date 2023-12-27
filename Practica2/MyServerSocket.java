ba9974fafe76c043b677d6e24e4f9bc1b85f01ae
Genis <145459716+gemigu@users.noreply.github.com>
2023-12-27 20:32:04 +0100

    Update MyServerSocket.java

--- Practica2/MyServerSocket.java
+++ Practica2/MyServerSocket.java
@@ -2,17 +2,14 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class MyServerSocket extends ServerSocket {
-
     MySocket mySocket;
     
     public MyServerSocket(int port) throws IOException {
-
         super(port);
     }
 
     @Override
     public MySocket accept() {
-
         try {
             mySocket = new MySocket(super.accept());
         } catch (IOException e) {
@@ -22,7 +19,6 @@ public class MyServerSocket extends ServerSocket {
     }
 
     public void close() {
-        
         try {
             super.close();
         } catch (IOException e) {
