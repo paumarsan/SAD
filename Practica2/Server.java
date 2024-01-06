08bb49c02b63d1ce6376d22c1a8ebb6ed6b6be72
Genis <145459716+gemigu@users.noreply.github.com>
2024-01-06 18:56:46 +0100

    Update Server.java

--- Practica2/Server.java
+++ Practica2/Server.java
@@ -1,74 +1,39 @@
-import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-public class MyServer {
-    
-    private static final ConcurrentHashMap<String, MySocket> chm = new ConcurrentHashMap<String, MySocket>();
-
+public class Server {
     public static void main(String[] args) {
+        Map<String, MySocket> mySocketMap = new ConcurrentHashMap<String,MySocket>();
+        MyServerSocket myServerSocket = new MyServerSocket(40000);
         
-        MyServerSocket ss = new MyServerSocket(Integer.parseInt(args[0]));
-        ExecutorService pool = Executors.newFixedThreadPool(50);
-        
-        while(true) {
-            pool.execute(new Task(ss.accept()));
-        }
-    }
+        while(true){
+            MySocket socket = myServerSocket.accept();
+            
+            new Thread() {
+                public void run() {
+                    try{
+                    String nick = socket.readLine();
+                    mySocketMap.put(nick,socket);
+                    System.out.println("\u001b[35mSuccessfully Connected\u001b[0m");
 
-    public static class Task implements Runnable {
-        private MySocket sc;
-        private String name;
-        private String msg;
-        public Task(MySocket s) {
-            sc = s;
-        }
-        @Override
-        public void run() {
-            while(true) {
-                try {
-                    sc.println("Introdueix nom d'usuari: ");
-                    name = sc.readLine();
-                    if (!chm.containsKey(name)) {
-                        chm.put(name, sc);
-                        System.out.println("USUARI: " + name);
-                        for(MySocket bs : chm.values()) {
-                            bs.println("--- " + name + " s'ha unit al chat" + " ---");
-                        }
-                        break;
-                    } else {
-                        sc.println("Nom d'usuari agafat. Proba altre nom.");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            while(sc != null) {
-                try {
-                    while((msg = sc.readLine()) != null) {
-                        for(MySocket bs : chm.values()) {
-                            if (sc != bs) {
-                                bs.println(name + ": " + msg);
+                    String line;
+                    while ((line = socket.readLine()) != null) {
+                        System.out.println("\u001b[33mThe server has received the message: \u001b[0m" + line);
+                        for (String n : mySocketMap.keySet()){
+                            if(n!=nick){
+                                mySocketMap.get(n).writeLine(nick+": "+line);
+                                System.out.println("\u001b[32mMessage resend to: \u001b[0m" + n);
                             }
-                        }
+                        }  
                     }
-                } catch (IOException e) {
+                    System.out.println("\u001b[35mClosed Connection: \u001b[0m"+nick);
+                    mySocketMap.remove(nick);
+                    socket.close();
+                }catch(Exception e){
                     e.printStackTrace();
                 }
-                break;
-            }
-            System.out.println("USUARI DESCONNECTAT: " + name);
-            chm.remove(name);
-            try {
-                for(MySocket bs : chm.values()) {
-                    if (sc != bs) {
-                        bs.println("--- " + name + " ha abandonat el chat" + " ---");
-                    }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+            }.start(); 
     }
+}
 }
