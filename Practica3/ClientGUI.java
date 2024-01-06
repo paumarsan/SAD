406555e841bac51eadef6868817614f88203c69d
Genis <145459716+gemigu@users.noreply.github.com>
2024-01-06 21:59:39 +0100

    Update ClientGUI.java

--- Practica3/ClientGUI.java
+++ Practica3/ClientGUI.java
@@ -2,14 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
 public class ClientGUI {
     private JTextField messageField;
     private JTextArea chatArea;
     private Client client;
 
-    public ClientGUI() {
+    public ClientGUI(Client client) {
+        this.client = client;
+
         JFrame frame = new JFrame("Chat Client");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
@@ -37,41 +38,17 @@ public class ClientGUI {
         frame.add(inputPanel, BorderLayout.SOUTH);
 
         frame.setVisible(true);
-
-        client = new Client();
-        initClient();
     }
 
-    private void initClient() {
-        String nick = JOptionPane.showInputDialog("Introduce tu nombre:");
-        client.writeLine(nick);
-
-        new Thread(() -> {
-            while (true) {
-                String message = client.readLine();
-                appendMessage(message);
-            }
-        }).start();
+    public void appendMessage(String message) {
+        chatArea.append(message + "\n");
     }
 
     private void sendMessage() {
         String message = messageField.getText();
         if (!message.isEmpty()) {
-            client.writeLine(message);
+            client.sendMessage(message);
             messageField.setText("");
         }
-    }
-
-    private void appendMessage(String message) {
-        chatArea.append(message + "\n");
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new ClientGUI();
-            }
-        });
     }
 }
