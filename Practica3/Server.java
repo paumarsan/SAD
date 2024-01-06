c516966042b2d14a76db3c7b89636f88eb93bd91
Genis <145459716+gemigu@users.noreply.github.com>
2024-01-06 21:49:44 +0100

    Update Server.java

--- Practica3/Server.java
+++ Practica3/Server.java
@@ -11,28 +11,28 @@ public class Server {
             
             new Thread() {
                 public void run() {
-                    try{
-                    String nick = socket.readLine();
-                    mySocketMap.put(nick,socket);
-                    System.out.println("Connexio Realitzada");
+                    try {
+                        String nick = socket.readLine();
+                        mySocketMap.put(nick, socket);
+                        System.out.println("Connexio Realitzada");
 
-                    String line;
-                    while ((line = socket.readLine()) != null) {
-                        System.out.println(line);
-                        for (String n : mySocketMap.keySet()){
-                            if(n!=nick){
-                                mySocketMap.get(n).writeLine(nick+": "+line);
+                        String line;
+                        while ((line = socket.readLine()) != null) {
+                            System.out.println(line);
+                            for (String n : mySocketMap.keySet()) {
+                                if (!n.equals(nick)) {
+                                    mySocketMap.get(n).writeLine(nick + ": " + line);
+                                }
                             }
-                        }  
+                        }
+                        System.out.println("Connexio tancada: " + nick);
+                        mySocketMap.remove(nick);
+                        socket.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                    System.out.println("Connexio tancada: "+nick);
-                    mySocketMap.remove(nick);
-                    socket.close();
-                }catch(Exception e){
-                    e.printStackTrace();
                 }
-                }
-            }.start(); 
+            }.start();
+        }
     }
-}
 }
