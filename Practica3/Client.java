3d17f3ae62885fd1fdf21c13d252b401ef922898
Genis <145459716+gemigu@users.noreply.github.com>
2024-01-06 17:23:16 +0100

    Update Client.java

--- Practica3/Client.java
+++ Practica3/Client.java
@@ -1,1 +1,32 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Client {
+
+  public static final int PORT = 5000;
+
+  public static void main(String[] args) throws IOException {
+    MySocket mySocket = new MySocket("localhost", PORT);
+
+    new Thread(() -> {
+        String input;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+          try {
+            while ((input = in.readLine()) != null) {
+              mySocket.printLine(input);
+          }
+          mySocket.close(); 
+        }catch(IOException e) {
+              e.printStackTrace();
+        }
+    }).start(); 
+    
+    new Thread(() -> { 
+        String output;
+        while ((output = mySocket.readLine()) != null) {  
+          System.out.println(output);
+      	}
+    }).start();
+  }
+}
