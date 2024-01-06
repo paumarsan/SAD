74386078ff7222fd63dc82c82eb302e50da732c6
Genis <145459716+gemigu@users.noreply.github.com>
2024-01-06 19:17:55 +0100

    Update Server.java

--- Practica2/Server.java
+++ Practica2/Server.java
@@ -14,19 +14,18 @@ public class Server {
                     try{
                     String nick = socket.readLine();
                     mySocketMap.put(nick,socket);
-                    System.out.println("\u001b[35mSuccessfully Connected\u001b[0m");
+                    System.out.println("Connexio Realitzada");
 
                     String line;
                     while ((line = socket.readLine()) != null) {
-                        System.out.println("\u001b[33mThe server has received the message: \u001b[0m" + line);
+                        System.out.println(line);
                         for (String n : mySocketMap.keySet()){
                             if(n!=nick){
                                 mySocketMap.get(n).writeLine(nick+": "+line);
-                                System.out.println("\u001b[32mMessage resend to: \u001b[0m" + n);
                             }
                         }  
                     }
-                    System.out.println("\u001b[35mClosed Connection: \u001b[0m"+nick);
+                    System.out.println("Connexio tancada: "+nick);
                     mySocketMap.remove(nick);
                     socket.close();
                 }catch(Exception e){
